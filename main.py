64d9870da939f708d1d1035e2cbbedfbc45cd61f
Aishwarya Shankar <aishwaryashankar17@gmail.com>
2022-05-05 19:59:16 -0700

    All API's completed (15/15) and tested in sharded cluster

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 # Application attempts database connection and accordingly returns success or error message
 try:
-    client = MongoClient(host="3.80.81.163:27020")
+    client = MongoClient(host="54.147.160.117:27020")
     client.server_info()
     db = client.projectdb
     print("Successfully connected to database ", db)
@@ -19,9 +19,9 @@ except:
     print("ERROR - could not make connection to database.")
 
 
-# function add_review() adds a movie review to the database and accordingly returns success or error message
+# function 'create' adds a movie review to the database and accordingly returns success or error message
 @app.route('/create', methods=['POST'])
-def add_review():
+def create():
     try:
         req = request.json
         review_id = req['review_id']
@@ -35,8 +35,7 @@ def add_review():
         helpful = req['helpful']
 
         insert_id = db.reviews.insert_one({'review_id':review_id, 'reviewer':reviewer, 'movie':movie, 'rating':rating, 'review_summary':review_summary, 'review_date':review_date,'spoiler_tag':spoiler_tag,'review_detail':review_detail, "helpful":helpful})
-        print(id)
-        respond = jsonify("Successfully added movie review.")
+        respond = jsonify("Successfully added movie review with review_id: ", review_id)
         respond.status_code = 200
         return respond
     except:
@@ -45,9 +44,9 @@ def add_review():
         return respond
 
 
-# Function update_review() updates a movie review in database which has the specified review_id and reviewer field values
+# Function 'update' updates a movie review in database which has the specified review_id and reviewer field values
 @app.route('/update/<review_id>/<reviewer>', methods=['PUT'])
-def update_review(review_id, reviewer):
+def update(review_id, reviewer):
     try:
         rev_id = review_id
         rev_person = reviewer
@@ -75,9 +74,9 @@ def update_review(review_id, reviewer):
         return respond
 
 
-# Function delete_review() deletes a movie review in the database which has the specified review_id and reviewer field values
+# Function 'delete' deletes a movie review in the database which has the specified review_id and reviewer field values
 @app.route('/delete/<review_id>/<reviewer>', methods=['DELETE'])
-def delete_review(review_id, reviewer):
+def delete(review_id, reviewer):
     try:
         db.reviews.delete_one({'review_id': review_id,'reviewer':reviewer})
         respond = jsonify("Successfully deleted movie review with id ", review_id)
@@ -103,12 +102,11 @@ def get_ten_reviews():
         return respond
 
 
-# Function get_num_reviews() retrieves the number of distinct movies reviewed in the database
-@app.route('/count', methods=['GET'])
+# Function 'get_num_reviews' retrieves the number of distinct movies reviewed in the database
+@app.route('/get_num_reviews', methods=['GET'])
 def get_num_reviews():
     try:
         num_reviews = len(db.reviews.distinct("movie"))
-        print("Number of distinct movies reviewed in database: ",num_reviews)
         respond = jsonify("Successfully retrieved number of distinct movies in database: ", num_reviews)
         respond.status_code = 200
         return respond
@@ -117,8 +115,8 @@ def get_num_reviews():
         respond.status_code = 400
         return respond
 
-# Function get_ten_stars() retrieves the movie titles in the database with 10-star ratings
-@app.route('/tenstars', methods=['GET'])
+# Function 'get_ten_stars' retrieves the movie titles in the database with 10-star ratings
+@app.route('/get_ten_stars', methods=['GET'])
 def get_ten_stars():
     try:
         ten_star_titles = db.reviews.find({"rating":"10"},{"movie":1}).distinct("movie")
@@ -129,12 +127,12 @@ def get_ten_stars():
         respond.status_code = 400
         return respond
 
-# Function get_by_reviewer() retrieves reviews created by a specific reviewer
-@app.route('/reviewer/<reviewer>', methods=['GET'])
+# Function 'get_by_reviewer' retrieves reviews created by a specific reviewer
+@app.route('/get_by_reviewer/<reviewer>', methods=['GET'])
 def get_by_reviewer(reviewer):
 
     try:
-        by_reviewer = db.reviews.find({"reviewer":reviewer},{"movie":1,"reviewer":1,"review_detail":1, "_id":0})
+        by_reviewer = db.reviews.find({"reviewer":reviewer},{"movie":1,'_id':0})
         respond = dumps(by_reviewer)
         return respond
     except:
@@ -142,7 +140,7 @@ def get_by_reviewer(reviewer):
         respond.status_code = 400
         return respond
 
-# Function love_reviews() retrieves all movies in the database which contain the word "love" in the title
+# Function 'love_reviews' retrieves all movies in the database which contain the word "love" in the title
 @app.route('/love_movies', methods=['GET'])
 def love_reviews():
     try:
@@ -154,11 +152,11 @@ def love_reviews():
         respond.status_code = 400
         return respond
 
-# Function spoiler_free() retrieves all spoiler-free reviews for a specified movie title
-@app.route('/no_spoiler/<title>', methods=['GET'])
+# Function 'spoiler_free' retrieves all spoiler-free reviews for a specified movie title
+@app.route('/spoiler_free/<title>', methods=['GET'])
 def spoiler_free(title):
     try:
-        no_spoilers = db.reviews.find({"movie":title, "spoiler_tag":0},{"_id":0})
+        no_spoilers = db.reviews.find({"movie":title, "spoiler_tag":0})
         respond = dumps(no_spoilers)
         return respond
 
@@ -167,12 +165,12 @@ def spoiler_free(title):
         respond.status_code = 400
         return respond
 
-# Function get_genre() retrieves reviews whose review_summary contains the name of a specific genre (ie. crime)
-@app.route('/genre/<genre>', methods=['GET'])
+# Function 'get_by_genre' retrieves reviews whose review_summary contains the name of a specific genre (ie. crime)
+@app.route('/get_by_genre/<genre>', methods=['GET'])
 def get_genre(genre):
     try:
         genre_string = '.*'+genre+".*"
-        contains_genre = db.reviews.find({"review_summary":{'$regex':genre_string, '$options':'-i'}},{"_id":0})
+        contains_genre = db.reviews.find({"review_summary":{'$regex':genre_string, '$options':'-i'}})
         respond = dumps(contains_genre)
         return respond
 
@@ -181,8 +179,8 @@ def get_genre(genre):
         respond.status_code = 400
         return respond
 
-# Function get_actor() retrieves movie titles whose review_summary mentions the specified actor or actress
-@app.route('/actor/<actor>', methods=['GET'])
+# Function 'get_by_actor' retrieves movie titles whose review_summary mentions the specified actor or actress
+@app.route('/get_by_actor/<actor>', methods=['GET'])
 def get_actor(actor):
     try:
         actor_string = '.*'+actor+".*"
@@ -196,6 +194,69 @@ def get_actor(actor):
         return respond
 
 
+# Function 'x_most_popular' retrieves the "x" most popular movies based on the number of reviews
+@app.route('/x_most_popular/<x>', methods=['GET'])
+def x_most_popular(x):
+    try:
+        x = int(x)
+        cursor = db.reviews.aggregate([{'$match':{}}, {'$sortByCount':"$movie"},{'$limit':x}])
+        respond = dumps(cursor)
+        return respond
+
+    except:
+        respond = jsonify("ERROR - could not retrieve the specified number of most popular movies.")
+        respond.status_code = 400
+        return respond
+
+# Function 'release_range' retrieves all movies that released within a two-year time range (ie. between 2018 and 2019)
+@app.route('/release_range/<first>/<second>', methods=['GET'])
+def release_range(first, second):
+    try:
+       year1 = ".*"+first+".*"
+       year2 = ".*"+second+".*"
+       movies = db.reviews.find({"$or":[{"movie":{"$regex":year1}},{"movie":{"$regex":year2}}]},{"movie":1,"_id":0}).distinct("movie")
+       respond = dumps(movies)
+       return respond
+    except:
+        respond = jsonify("ERROR - could not retrieve movies within the specified year range.")
+        respond.status_code = 400
+        return respond
+
+# Function 'avg_by_reviewer' computes the average rating given for all movies reviewed by a specified reviewer
+@app.route('/avg_by_reviewer/<reviewer>', methods=['GET'])
+def avg_by_reviewer(reviewer):
+    try:
+        cursor = db.reviews.aggregate([{"$match": {"reviewer": reviewer}}, {"$group": {"_id": "$reviewer", "Average":{"$avg": {'$toInt':'$rating'}}}}])
+        respond = dumps(cursor)
+        return respond
+    except:
+        respond = jsonify("ERROR - could not compute average rating given by specified reviewer.")
+        respond.status_code = 400
+        return respond
+
+# Function 'helpfulness' counts how many people found a specified reviewer's reviews helpful in total
+@app.route('/helpfulness/<reviewer>', methods=['GET'])
+def helpfulness(reviewer):
+    try:
+        cursor = db.reviews.aggregate([{'$match':{'reviewer':reviewer}},{'$group':{'_id':'$reviewer', 'helpfulness_total':{'$sum':{'$toInt':{'$first':'$helpful'}}}}}])
+        respond = dumps(cursor)
+        return respond
+    except:
+        respond = jsonify("ERROR - could not count how many people found specified reviewer's reviews helpful.")
+        respond.status_code = 400
+        return respond
+
+# Function 'avg_movie_rating' computes the average rating for a specified movie title
+@app.route('/avg_movie_rating/<movie>', methods=['GET'])
+def avg_movie_rating(movie):
+    try:
+        cursor = db.reviews.aggregate([{'$match':{'movie':movie}},{'$group':{'_id':'$movie','Average':{'$avg':{'$toInt':'$rating'}}}}])
+        respond = dumps(cursor)
+        return respond
+    except:
+        respond = jsonify("ERROR - could not compute the average rating for the specified movie.")
+        respond.status_code = 400
+        return respond
 
 if __name__ == "__main__":
     app.run(debug=True)
